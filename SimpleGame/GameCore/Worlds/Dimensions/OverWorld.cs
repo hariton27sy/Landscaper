9787d1b72a0110ffb830c2e71ebb3079c9c8c17f
Sergey <hariton27sy@gmail.com>
2020-05-28 12:07:17 +0500

    Return old collision method

--- SimpleGame/GameCore/Worlds/Dimensions/OverWorld.cs
+++ SimpleGame/GameCore/Worlds/Dimensions/OverWorld.cs
@@ -42,9 +42,11 @@ namespace SimpleGame.GameCore.Worlds
 
         public void Update(TimeSpan delta)
         {
-            player.Velocity -= Vector3.UnitY * gravity * (float)delta.TotalSeconds;
+            if (GetBlockId(player.Position - Vector3.UnitY) == 0)
+                player.Velocity -= Vector3.UnitY * gravity * (float)delta.TotalSeconds;
             // player.Position += player.AbsoluteVelocity * (float) delta.TotalSeconds;
-            TryMove(player, player.AbsoluteVelocity * (float)delta.TotalSeconds);
+            if (Math.Abs(player.AbsoluteVelocity.Length) > 1e-10)
+                TryMove(player, player.AbsoluteVelocity * (float)delta.TotalSeconds);
             // if (player.Position.Y < 0)
             //     player.Position = new Vector3(player.Position.X, 0, player.Position.Z);
         }
@@ -65,53 +67,22 @@ namespace SimpleGame.GameCore.Worlds
             bb.End = new Vector3(x + blockThickness, y + blockThickness, z + blockThickness);
             return bb;
         }
-        private BoundaryBox? GetNearestBlock(BoundaryBox boundaryBox, Vector3 delta)
+        private Vector3? GetNearestBlock(Vector3 startPos, Vector3 delta)
         {
-            var newPos = boundaryBox + delta;
-
-            var result = newPos.GetVertices()
-                .Where(v => GetBlockId(v) != 0)
-                .Select(BoundaryBox.IntBoxFromVector)
-                .OrderBy(b => boundaryBox.DistanceTo(b))
-                .FirstOrDefault();
-            return result.Start == Vector3.Zero && result.End == Vector3.Zero ? (BoundaryBox?) null : result;
             
-            
-            const int partitions = 10;
-            for (int i = 0; i < partitions; i++)
+            var normDelta = Vector3.Normalize(delta) * 0.2f;
+            var result = normDelta;
+            var prevLength = delta.Length;
+            while ((delta - result).Length < prevLength)
             {
-                foreach (var vertex in boundaryBox.GetVertices())
+                prevLength = (delta - result).Length;
+                if (GetBlockId(startPos + result) != 0)
                 {
-                    var offset = (delta / partitions * i) + vertex;
-                    for (int x = (int) vertex.X; x < offset.X; x++)
-                    {
-                        for (int y = (int) vertex.Y; y < offset.Y; y++)
-                        {
-                            for (int z = (int) vertex.Z; z < offset.Z; z++)
-                            {
-                                // Console.WriteLine((offset, x, y, z));
-                                if (IsInBlock(offset, x, y, z))
-                                {
-                                    var chunkPosition = new Vector3(x, y, z).ToChunkPosition();
-                                    var chunk = GetChunk(chunkPosition);
-
-                                    try
-                                    {
-                                        if (chunk.Map[x, y, z] != 0)
-                                        {
-                                            return BlockBoundary(x, y, z);
-                                        }
-                                    }
-                                    catch
-                                    {
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    result = startPos + result;
+                    return new Vector3((int) result.X, (int) result.Y, (int) result.Z);
                 }
+                result += delta;
             }
-
             return null;
         }
 
@@ -132,7 +103,7 @@ namespace SimpleGame.GameCore.Worlds
             try
             {
                 var chunk = GetChunk(new Vector2(chunkX, chunkZ));
-                return chunk?.Map[x, y, z] ?? 0;
+                return chunk?.Map[x, y, z] ?? 1;
             }
             catch (IndexOutOfRangeException)
             {
@@ -153,35 +124,55 @@ namespace SimpleGame.GameCore.Worlds
         
         private void TryMove(IPlayer person, Vector3 delta)
         {
-            BoundaryBox? nearest;
-            var prevNearest = new BoundaryBox();
-            while ((nearest = GetNearestBlock(GetPersonBoundaryBox(person.Position), delta)) != null && 
-                   !((BoundaryBox) nearest).Equals(prevNearest))
+            Vector3? nearest;
+            Vector3 prevNearest = Vector3.Zero;
+            while ((nearest = GetNearestBlock(person.Position, delta)) != null && prevNearest != nearest)
             {
                 // Console.WriteLine($"Nearest {nearest}");
-                var correctDelta = CorrectDelta(player.BoundaryBox, delta, (BoundaryBox) nearest);
-                if (Math.Abs(correctDelta.Y - delta.Y) > 1e-10)
-                        person.Velocity = new Vector3(person.Velocity.X, 0, person.Velocity.Z);
-                delta = correctDelta;
+                delta = CorrectDelta(player.Position, delta, (Vector3) nearest);
+                prevNearest = (Vector3)nearest;
             }
 
             person.Position += delta;
         }
 
-        private Vector3 CorrectDelta(BoundaryBox startPos, Vector3 delta, BoundaryBox blockPos)
+        private Vector3 CorrectDelta(Vector3 startPos, Vector3 delta, Vector3 blockPos)
         {
-            var deltaX = GetDeltaAlongAxis(v => v.X, delta, startPos, blockPos);
-            var deltaY = GetDeltaAlongAxis(v => v.Y, delta, startPos, blockPos);
-            var deltaZ = GetDeltaAlongAxis(v => v.Z, delta, startPos, blockPos);
+            var epsilon = 0.2f;
+            var x = blockPos.X;
+            var y = startPos.Y +  delta.Y / delta.X * (x - startPos.X);
+            var z = startPos.Z + delta.Z / delta.X * (x - startPos.X);
+            if (delta.X > 0 && (int)y == (int) blockPos.Y && (int) z == (int) blockPos.Z)
+                return new Vector3(x - startPos.X - epsilon, delta.Y, delta.Z);
+            x = x + 1;
+            y = startPos.Y +  delta.Y / delta.X * (x - startPos.X);
+            z = startPos.Z + delta.Z / delta.X * (x - startPos.X);
+            if (delta.X < 0 && (int) y == (int) blockPos.Y && (int) z == (int) blockPos.Z)
+                return new Vector3(x - startPos.X + epsilon, delta.Y, delta.Z);
             
-            if (Math.Abs(deltaY * delta.Y) < Math.Abs(deltaX * delta.X) &&
-                Math.Abs(deltaZ * delta.Z) < Math.Abs(deltaX * delta.X))
-                return new Vector3(deltaX * Math.Sign(delta.X), delta.Y, delta.Z);
+            y = blockPos.Y;
+            x = startPos.X +  delta.X / delta.Y * (y - startPos.Y);
+            z = startPos.Z +  delta.Z / delta.Y * (y - startPos.Y);
+            if (delta.Y > 0 && (int)x == (int) blockPos.X && (int) z == (int) blockPos.Z)
+                return new Vector3(delta.X, y - startPos.Y - epsilon, delta.Z);
+            y = y + 1;
+            x = startPos.X +  delta.X / delta.Y * (y - startPos.Y);
+            z = startPos.Z +  delta.Z / delta.Y * (y - startPos.Y);
+            if (delta.Y < 0 && (int) x == (int) blockPos.X && (int) z == (int) blockPos.Z)
+                return new Vector3(delta.X, y - startPos.Y + epsilon, delta.Z);
             
-            if (Math.Abs(deltaY * delta.Y) < Math.Abs(deltaZ * delta.Z))
-                return new Vector3(delta.X, delta.Y, deltaZ * Math.Sign(delta.Z));
-            
-            return new Vector3(delta.X, deltaY * Math.Sign(delta.Y), delta.Z);
+            z = blockPos.Z;
+            x = startPos.X +  delta.X / delta.Z * (z - startPos.Z);
+            y = startPos.Y +  delta.Y / delta.Z * (z - startPos.Z);
+            if (delta.Z > 0 && (int)x == (int) blockPos.X && (int) y == (int) blockPos.Y)
+                return new Vector3(delta.X, delta.Y, z - startPos.Z - epsilon);
+            z = z + 1;
+            x = startPos.X +  delta.X / delta.Z * (z - startPos.Z);
+            y = startPos.Y +  delta.Y / delta.Z * (z - startPos.Z);
+            if (delta.Z < 0 && (int) x == (int) blockPos.X && (int) y == (int) blockPos.Y)
+                return new Vector3(delta.X, delta.Y, z - startPos.Z + epsilon);
+
+            return delta;
         }
 
         private float GetDeltaAlongAxis(Func<Vector3, float> axisSelector, Vector3 delta, BoundaryBox startPos,
